495871d3bb4ae2aea5af142a6a7a2dad11aac26c
bottkars <karsten.bott@bottnet.de>
2020-06-15 11:39:27 +0200

    added params

--- vsphere/scripts/clone-node.sh
+++ vsphere/scripts/clone-node.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-set 
+set -eux
+govc about
 
-lala
-exit 1
+echo "==>Beginning deployment of ${LABBUILDR_VM_NAME} in ${LABBUILDR_RESOURCE_POOL}"
+govc vm.clone -vm 2019_template \
+ -m=${LABBUILDR_VM_MEMORY} \
+ -c=${LABBUILDR_VM_CPU} \
+ -net=${LABBUILDR_NETWORK} \
+ -pool=${LABBUILDR_RESOURCE_POOL} \
+ -folder=${LABBUILDR_FOLDER} \
+ ${LABBUILDR_VM_NAME} 
