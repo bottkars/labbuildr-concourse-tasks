950717a77640472e267f48a9fc6832cd770df77d
bottkars <karsten.bott@bottnet.de>
2020-06-16 10:27:34 +0200

    last

--- vsphere/scripts/configure-dcnode.sh
+++ vsphere/scripts/configure-dcnode.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -eu
 govc about
+GUEST_SCRIPT_DIR="D:/labbuildr-scripts/dcnode"
+GUEST_SHELL="C:/Windows/System32/WindowsPowerShell/V1.0/powershell.exe"
 # we need to put in a wait for vm up and running ?!
+printf "==>Waiting for ${LABBUILDR_VM_NAME} to become ready"
+until govc guest.start -l="Administrator:Password123!" \
+    -vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" "${VM_SHELL}" > /dev/null 2>&1
+do
+  printf ". "
+  sleep 5
+done
+echo
 
 echo "==>Beginning Configuration of ${LABBUILDR_VM_NAME} for ${LABBUILDR_FQDN}"
 DEBIAN_FRONTEND=noninteractive apt-get install -qq genisoimage < /dev/null > /dev/null
@@ -9,12 +19,13 @@ genisoimage -o labbuildr-scripts.iso -R -J -D labbuildr-scripts
 echo "==>Uploading Script ISO"
 govc datastore.upload -ds $LABBUILDR_DATASTORE ./labbuildr-scripts.iso ${LABBUILDR_VM_NAME}/labbuildr-scripts.iso 
 echo "==>Attaching Script ISO"
-govc device.cdrom.insert -vm.ipath ${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME} \
--device cdrom-3000 ${LABBUILDR_VM_NAME}/labbuildr-scripts.iso 
+govc device.cdrom.insert \
+    -vm.ipath ${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME} \
+    -device cdrom-3000 ${LABBUILDR_VM_NAME}/labbuildr-scripts.iso 
 
-govc device.connect -vm.ipath=${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME} cdrom-3000
-GUEST_SCRIPT_DIR="D:/labbuildr-scripts/dcnode"
-GUEST_SHELL="C:/Windows/System32/WindowsPowerShell/V1.0/powershell.exe"
+govc device.connect \
+        -vm.ipath=${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME} cdrom-3000
+
 GUEST_SCRIPT="new-dc.ps1"
 GUEST_PARAMETERS="-dcname ${LABBUILDR_VM_NAME} -Domain ${LABBUILDR_FQDN} -AddressFamily IPv4 -IPv4Subnet ${LABBUILDR_SUBNET}"
 govc guest.start -l="Administrator:Password123!" \
@@ -23,7 +34,8 @@ govc guest.start -l="Administrator:Password123!" \
 
 
 printf "checking for Step 2 "
-until govc guest.run -l Administrator:Password123! -vm=dcnode $GUEST_SHELL  "-command get-item c:/scripts/2.pass" > /dev/null 2>&1
+until govc guest.run -l Administrator:Password123! \
+    -vm=dcnode $GUEST_SHELL  "-command get-item c:/scripts/2.pass" > /dev/null 2>&1
 do
   printf ". "
   sleep 5
@@ -35,11 +47,12 @@ LABBUILDR_DOMAIN_SUFFIX=$(echo $LABBUILDR_FQDN | cut -d'.' -f2-)
 GUEST_SCRIPT="finish-domain.ps1"
 GUEST_PARAMETERS="-domain ${LABBUILDR_DOMAIN} -domainsuffix ${LABBUILDR_DOMAIN_SUFFIX}"
 govc guest.start -l="Administrator:Password123!" \
--vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
-"${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
+    -vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
+    "${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
 
 printf "checking for Step 3 "
-until govc guest.run -l Administrator:Password123! -vm=dcnode $GUEST_SHELL  "-command get-item c:/scripts/3.pass" > /dev/null 2>&1
+until govc guest.run -l Administrator:Password123! \
+    -vm=dcnode $GUEST_SHELL  "-command get-item c:/scripts/3.pass" > /dev/null 2>&1
 do
   printf ". "
   sleep 5
@@ -50,8 +63,8 @@ echo
 GUEST_SCRIPT="dns.ps1"
 GUEST_PARAMETERS="-IPv4subnet ${LABBUILDR_SUBNET} -IPv4Prefixlength 24 -AddressFamily IPv4"
 govc guest.run -l="Administrator:Password123!" \
--vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
-"${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
+    -vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
+    "${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
 
 echo "==>Running DCNode Customization for vSphere"
 GUEST_SCRIPTS=("add-serviceuser.ps1" "pwpolicy.ps1")
