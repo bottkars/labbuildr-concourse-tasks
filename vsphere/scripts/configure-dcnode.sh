719b1bfba9c8ababf034d3eef973e336b46f3708
bottkars <karsten.bott@bottnet.de>
2020-06-16 09:23:46 +0200

    added remove script

--- vsphere/scripts/configure-dcnode.sh
+++ vsphere/scripts/configure-dcnode.sh
@@ -12,14 +12,75 @@ govc device.cdrom.insert -vm.ipath ${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME} \
 -device cdrom-3000 ${LABBUILDR_VM_NAME}/labbuildr-scripts.iso 
 
 govc device.connect -vm.ipath=${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME} cdrom-3000
-
+GUEST_SCRIPT_DIR="D:/labbuildr-scripts/dcnode"
 GUEST_SHELL="C:/Windows/System32/WindowsPowerShell/V1.0/powershell.exe"
-GUEST_SCRIPT="D:/labbuildr-scripts/dcnode/new-dc.ps1"
+GUEST_SCRIPT="new-dc.ps1"
 GUEST_PARAMETERS="-dcname ${LABBUILDR_VM_NAME} -Domain ${LABBUILDR_FQDN} -AddressFamily IPv4 -IPv4Subnet ${LABBUILDR_SUBNET}"
 govc guest.start -l="Administrator:Password123!" \
 -vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
-"${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
-# was: 			$script_invoke = $NodeClone | Invoke-VMXPowershell -Guestuser $Adminuser -Guestpassword $Adminpassword -ScriptPath $IN_Guest_UNC_ScenarioScriptDir -Script new-dc.ps1 -Parameter "
-#was : -dcname $DCName -Domain $BuildDomain -IPv4subnet $IPv4subnet -IPv4Prefixlength $IPv4PrefixLength -IPv6PrefixLength $IPv6PrefixLength -IPv6Prefix $IPv6Prefix  -AddressFamily $AddressFamily -TimeZone '$($labdefaults.timezone)' $AddGateway $CommonParameter
-#" -interactive -nowait
-exit 1
+"${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
+
+
+printf "checking for Step 2 "
+until govc guest.run -l Administrator:Password123! -vm=dcnode $GUEST_SHELL  "-command get-item c:/scripts/2.pass" > /dev/null 2>&1
+do
+  printf ". "
+  sleep 5
+done
+echo
+LABBUILDR_DOMAIN=$(echo $LABBUILDR_FQDN | cut -d'.' -f1-1)
+echo "==>Proceeding with Domain Initialization of ${LABBUILDR_DOMAIN}"
+LABBUILDR_DOMAIN_SUFFIX=$(echo $LABBUILDR_FQDN | cut -d'.' -f2-)
+GUEST_SCRIPT="finish-domain.ps1"
+GUEST_PARAMETERS="-domain ${LABBUILDR_DOMAIN} -domainsuffix ${LABBUILDR_DOMAIN_SUFFIX}"
+govc guest.start -l="Administrator:Password123!" \
+-vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
+"${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
+
+printf "checking for Step 3 "
+until govc guest.run -l Administrator:Password123! -vm=dcnode $GUEST_SHELL  "-command get-item c:/scripts/3.pass" > /dev/null 2>&1
+do
+  printf ". "
+  sleep 5
+done
+echo
+
+### no a little bit hacky before doing functions
+GUEST_SCRIPT="dns.ps1"
+GUEST_PARAMETERS="-IPv4subnet ${LABBUILDR_SUBNET} -IPv4Prefixlength 24 -AddressFamily IPv4"
+govc guest.run -l="Administrator:Password123!" \
+-vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
+"${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
+
+echo "==>Running DCNode Customization for vSphere"
+GUEST_SCRIPTS=("add-serviceuser.ps1" "pwpolicy.ps1")
+for GUEST_SCRIPT in "${GUEST_SCRIPTS[@]}"
+do
+echo "==>Running ${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT}"
+govc guest.run -l="Administrator:Password123!" \
+    -vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
+    "${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT}\""
+done
+
+
+
+echo "==>Running Node Customization for vSphere"
+NODE_SCRIPT_DIR="D:/labbuildr-scripts"
+GUEST_SCRIPT_DIR="D:/labbuildr-scripts/node"
+GUEST_SCRIPT="disable-hardening.ps1"
+GUEST_PARAMETERS="-UpdateType never"
+govc guest.run -l="Administrator:Password123!" \
+    -vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
+    "${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
+# might be delivered via "Scripts to Run"
+GUEST_SCRIPTS=("enable-ansiblewinrm.ps1" "set-winrm.ps1")
+GUEST_PARAMETERS="-ScriptDir ${NODE_SCRIPT_DIR}"
+for GUEST_SCRIPT in "${GUEST_SCRIPTS[@]}"
+do
+echo "==>Running ${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT}"
+govc guest.run -l="Administrator:Password123!" \
+    -vm.ipath="${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}" \
+    "${GUEST_SHELL}" "-Command \"${GUEST_SCRIPT_DIR}/${GUEST_SCRIPT} ${GUEST_PARAMETERS}\""
+done
+
+
---
+++ vsphere/scripts/delete-node.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+govc about
+
+echo "==>Beginning node removal of ${LABBUILDR_VM_NAME}"
+govc vm.destroy -vm.ipath  ${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}  
+echo "Cleaning Datastorre for VM ${LABBUILDR_VM_NAME}"
+
+govc datastore.rm -ds=${LABBUILDR_DATASTORE}  ${LABBUILDR_VM_NAME}
