6eb05f5f59436c805101529f8edbada396f72b85
bottkars <karsten.bott@bottnet.de>
2020-06-16 09:38:10 +0200

    exit 0

--- vsphere/scripts/delete-node.sh
+++ vsphere/scripts/delete-node.sh
@@ -7,3 +7,4 @@ govc vm.destroy -vm.ipath  ${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}
 echo "Cleaning Datastorre for VM ${LABBUILDR_VM_NAME}"
 
 govc datastore.rm -ds=${LABBUILDR_DATASTORE}  ${LABBUILDR_VM_NAME}
+exit 0
