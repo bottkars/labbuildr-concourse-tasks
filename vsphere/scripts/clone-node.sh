7d32ece486ed890514df7b7fc8b91479b2afc92d
bottkars <karsten.bott@bottnet.de>
2020-06-29 20:02:47 +0200

    added teplate version

--- vsphere/scripts/clone-node.sh
+++ vsphere/scripts/clone-node.sh
@@ -3,7 +3,7 @@ set -eu
 govc about
 
 echo "==>Beginning deployment of ${LABBUILDR_VM_NAME} in ${LABBUILDR_RESOURCE_POOL}"
-govc vm.clone -vm 2019_template \
+govc vm.clone -vm ${LABBUILDR_VM_TEMPLATE} \
  -m=${LABBUILDR_VM_MEMORY} \
  -c=${LABBUILDR_VM_CPU} \
  -net=${LABBUILDR_NETWORK} \
