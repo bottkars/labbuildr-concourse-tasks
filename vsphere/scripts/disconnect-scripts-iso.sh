6a72d5632df32c96f29e5c6e2983aff0f74ffc79
bottkars <karsten.bott@bottnet.de>
2020-07-01 07:11:32 +0200

    iso removal

--- vsphere/scripts/disconnect-scripts-iso.sh
+++ vsphere/scripts/disconnect-scripts-iso.sh
@@ -6,5 +6,5 @@ export LABBUILDR_VM_IPATH=${LABBUILDR_VM_FOLDER}/${LABBUILDR_VM_NAME}
 echo "==>Beginning node removal of ${LABBUILDR_DEVICE}"
 govc device.cdrom.eject -vm.ipath "${LABBUILDR_VM_IPATH}" -device ${LABBUILDR_DEVICE}
 echo "==>Removing ISO ${LABBUILDR_DATASTORE}/labbuildr-scripts.iso"
-govc datastore.rm -f -ds="${LABBUILDR_DATASTORE}/labbuildr-scripts.iso"  ${LABBUILDR_VM_NAME}
+govc datastore.rm -f -ds="${LABBUILDR_DATASTORE}" "${LABBUILDR_VM_NAME}/labbuildr-scripts.iso"
 exit 0
