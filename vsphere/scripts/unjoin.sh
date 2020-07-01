04b5fe5b75b52e59d25407e7516f1cce1ba64edd
bottkars <karsten.bott@bottnet.de>
2020-07-01 09:54:35 +0200

    last

--- vsphere/scripts/unjoin.sh
+++ vsphere/scripts/unjoin.sh
@@ -3,12 +3,11 @@ set -eux
 govc about
 export LABBUILDR_VM_IPATH=${LABBUILDR_VM_FOLDER}/${LABBUILDR_DCNODE}
 LABBUILDR_DOMAIN=$(echo $LABBUILDR_FQDN | cut -d'.' -f1-1)
+checktools
 export LABBUILDR_LOGINUSER="${LABBUILDR_DOMAIN}\\${LABBUILDR_LOGINUSER}"
 
 MYSELF="$(dirname "${BASH_SOURCE[0]}")"
 source "${MYSELF}/functions/labbuildr_functions.sh"
-
-checktools
 
 echo "Removing${LABBUILDR_VM_NAME} from ${LABBUILDR_DOMAIN}"
 
