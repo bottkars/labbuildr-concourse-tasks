b560450d9f035e21bc744d9b8b2bf6ce2fe8a561
bottkars <karsten.bott@dell.com>
2020-10-25 14:22:58 +0100

    deploy exchange

---
+++ vsphere/scripts/deploy-exchange.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu
+[[ "${LABBUILDR_DEBUG}" == "TRUE" ]] && set -x
+figlet labbuildr 2020
+govc about
+
+
+export LABBUILDR_DOMAIN=$(echo $LABBUILDR_FQDN | cut -d'.' -f1-1)
+export LABBUILDR_LOGINUSER="${LABBUILDR_DOMAIN}\\${LABBUILDR_LOGINUSER}"
+
+
+MYSELF="$(dirname "${BASH_SOURCE[0]}")"
+source "${MYSELF}/functions/labbuildr_functions.sh"
+
+vm_ready
+checktools
+SOURCE_DIR="c:\\swdist"
+
+
+
+echo "Setting Up Exchange 2019 $LABBUILDR_EXCHANGE_CU"
+GUEST_SCRIPT="${SCENARIO_SCRIPT_DIR}/install-exchange.ps1"
+GUEST_PARAMETERS="-ex_cu $LABBUILDR_EXCHANGE_CU -install_from ISO -Scriptdir ${GUEST_SCRIPT_DIR} -SourcePath $SOURCE_DIR"
+vm_powershell --SCRIPT "${GUEST_SCRIPT}"   \
+    --PARAMETERS "${GUEST_PARAMETERS}" --INTERACTIVE --NOWAIT
+
