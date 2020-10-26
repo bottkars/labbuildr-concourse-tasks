7033699a8d24bf5fa55d2c3ce0dc502103a6b99a
bottkars <karsten.bott@dell.com>
2020-10-26 09:31:14 +0100

    added create-user

---
+++ vsphere/scripts/create-user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+[[ "${LABBUILDR_DEBUG}" == "TRUE" ]] && set -x
+figlet labbuildr 2020
+govc about
+
+
+export LABBUILDR_DOMAIN=$(echo $LABBUILDR_FQDN | cut -d'.' -f1-1)
+export LABBUILDR_LOGINUSER="${LABBUILDR_DOMAIN}\\${LABBUILDR_LOGINUSER}"
+
+
+MYSELF="$(dirname "${BASH_SOURCE[0]}")"
+source "${MYSELF}/functions/labbuildr_functions.sh"
+
+vm_ready
+checktools
+SOURCE_DIR="c:\\swdist"
+
+
+echo "Setting Up Exchange 2019 DAG "
+GUEST_SCRIPT="${SCENARIO_SCRIPT_DIR}/user.ps1"
+GUEST_PARAMETERS="-ex_version E2019 -Scriptdir ${GUEST_SCRIPT_DIR} -SourcePath $SOURCE_DIR"
+vm_powershell --SCRIPT "${GUEST_SCRIPT}"   \
+    --PARAMETERS "${GUEST_PARAMETERS}" --INTERACTIVE
+
